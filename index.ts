104025dd1ffd4e6fa679c10f169f89061b7be835
Grasshooper <gautam.jha911@gmail.com>
2025-03-22 18:49:31 +0100

    init

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-import { registerRootComponent } from 'expo';
 import 'react-native-reanimated';
+import { registerRootComponent } from 'expo';
+import { AppRegistry } from 'react-native';
 
 import App from './App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+// Make sure to register the app component with both methods
+AppRegistry.registerComponent('main', () => App);
 registerRootComponent(App);
